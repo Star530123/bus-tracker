a71c435983c33d7b08e04ccc487d2bd2427dfe04
Yu-Xing <a0975708860@gmail.com>
2023-07-10 16:02:58 +0800

    feat: add another test case & edit requirements.txt

--- main.py
+++ main.py
@@ -1,29 +1,40 @@
+from dotenv import load_dotenv
+
+load_dotenv()
+
 from tdx_api.bus.v2.api import *
 from bus_tracker import BusTracker
 from user_setting import UserSetting
 import time
 import tdx_api.bus.enum as BusEnum
-from dotenv import load_dotenv
 
-load_dotenv()
+if __name__=='__main__':
+    bus_tracker = BusTracker()
+    setting = UserSetting(
+        username='YuXing', 
+        city=BusEnum.City.TPE.value, 
+        route=672, 
+        direction=BusEnum.Direction.INBOUND, 
+        target_stop='博仁醫院', 
+        before_target_stop=(3, 5)
+    )
+    setting2 = UserSetting(
+        username='Simon', 
+        city=BusEnum.City.TPE.value, 
+        route=214, 
+        direction=BusEnum.Direction.OUTBOUND, 
+        target_stop='光華商場', 
+        before_target_stop=(3, 7)
+    )
+    bus_tracker.subscribe_route_notification(setting)
+    bus_tracker.subscribe_route_notification(setting2)
 
-bus_tracker = BusTracker()
-setting = UserSetting(
-    username='YuXing', 
-    city=BusEnum.City.TPE.value, 
-    route=672, 
-    direction=BusEnum.Direction.INBOUND, 
-    target_stop='博仁醫院', 
-    before_target_stop=(3, 5)
-)
-bus_tracker.subscribe_route_notification(setting)
+    while bus_tracker.user_count != 0:
+        start = time.time()
+        bus_tracker.track()
+        end = time.time()
 
-while bus_tracker.user_count != 0:
-    start = time.time()
-    bus_tracker.track()
-    end = time.time()
+        print('{t} seconds/track'.format(t=end-start))
+        time.sleep(5)
 
-    print('{t} seconds/track'.format(t=end-start))
-    time.sleep(5)
-
-print('All users have notified. Stop the program.')
+    print('All users have notified. Stop the program.')
